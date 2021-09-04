15cba59e6d6f480b3b8a775f017b326121caf194
crycetruly <crycetruly@gmail.com>
2021-09-04 20:54:25 +0300

    Add all code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
+
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
